fc77de3506f35279b1caaae4c19645c9a19d77c8
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-01-30 10:29:41 -0300

    Update Classe.java

--- Classe/Classe.java
+++ Classe/Classe.java
@@ -6,26 +6,10 @@ public class Classe {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
         JoJo j1 = new JoJo();
-        while(true){
-            System.out.print("Digite seu nome: ");
-            String j1_nome = teclado.nextLine();
-            if (j1_nome.isEmpty()){
-                System.out.print("O nome não pode ficar vazio, refaça corretamente a operação.\n");
-            } else {
-                j1.setNome(teclado.nextLine());
-                break;
-            }       
-        }
-        while(true){
-            System.out.print("Digite o stand de " + j1.getNome() + ": ");
-            String j1_stand = teclado.nextLine();
-            if (j1_stand.isEmpty()){
-                System.out.print("O stand de " + j1.getNome() + " não pode ficar vazio, refaça corretamente a operação.\n");   
-            } else {
-                j1.setStand(teclado.nextLine());
-                break;
-            }  
-        }
+        System.out.print("Digite seu nome: ");
+        j1.setNome(teclado.nextLine());
+        System.out.print("Digite o stand de " + j1.getNome() + ": ");
+        j1.setStand(teclado.nextLine());
         j1.Interacao();
     }
 }
