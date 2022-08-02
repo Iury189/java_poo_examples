c3f32e93bad82b238737b28c93635a674e9bc6f8
Iury Fernandes <63120240+Iury189@users.noreply.github.com>
2022-08-02 09:03:13 -0300

    Update Aguia.java

--- Interface/Aguia.java
+++ Interface/Aguia.java
@@ -3,7 +3,7 @@ package pkginterface;
 public class Aguia implements Predador {
 
     @Override
-    public void Cacar() {
+    public void cacar() {
         System.out.println("A águia está caçando o sapo.");   
     }
     
